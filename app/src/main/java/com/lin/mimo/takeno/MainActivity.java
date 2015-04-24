af572a9887a4ff5b3dcaf8e9dc5310b2bd1d29bf
mimo <mimowulin@gmail.com>
2015-04-24 21:30:23 +0800

    主畫面設計

--- app/src/main/java/com/lin/mimo/takeno/MainActivity.java
+++ app/src/main/java/com/lin/mimo/takeno/MainActivity.java
@@ -1,17 +1,62 @@
 package com.lin.mimo.takeno;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Toolbar toolbar = (Toolbar)findViewById(R.id.toobar);
+        Toolbar toolview = (Toolbar)findViewById(R.id.toview);
+        toolbar.setTitle("主ToolBar");
+        toolview.setTitle("副ToolBar");
+        setSupportActionBar(toolbar);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toolbar.setTitle("按鈕有效");
+            }
+        });
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch(menuItem.getItemId()){
+                    case R.id.action_settings:
+                        Toast.makeText(MainActivity.this,"進入設定帳號ACTIVITY",Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        // 實作 drawer toggle 並放入 toolbar
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close);
+        mDrawerToggle.syncState();
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
     }
 
 
--- app/src/main/java/com/lin/mimo/takeno/ParseApplication.java
+++ app/src/main/java/com/lin/mimo/takeno/ParseApplication.java
@@ -3,20 +3,18 @@ package com.lin.mimo.takeno;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseACL;
-import com.parse.ParseObject;
+
 
 /**
  * Created by mimo on 2015/4/24.
  */
 public class ParseApplication extends Application {
-     String aid = getString(R.string.ApplicationID);
-    String cid = getString(R.string.ClientKey);
+
     @Override
     public void onCreate() {
         super.onCreate();
 
-        Parse.initialize(this,aid, cid);
+        Parse.initialize(this, "niheH2SIhwCynAHlVJG5yPGYhjG6qaeXvB5FEuD9","iL9YtVzG34IShz5hPSusDUKqU1VqvChwHtkNb8DV");
 
 
             }
